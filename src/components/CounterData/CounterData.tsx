74be2751d2217bcede31e18b959d239c851c044f
ivantm0 <iterolmartinez@gmail.com>
2025-04-03 12:22:34 +0200

    fix: fix counter data

--- src/components/CounterData/CounterData.tsx
+++ src/components/CounterData/CounterData.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 
+import { useInView } from "react-intersection-observer";
 import CountUp from "react-countup";
 import MotionTransition from "../MotionTransition/MotionTransition";
 import { dataCounter } from "./CounterData.data";
@@ -9,16 +10,22 @@ const CounterData = () => {
         <MotionTransition className="max-w-5xl py-10 mx-auto md:py-64">
             <div className="justify-between md:flex">
                 {dataCounter.map(({ id, startNumber, endNumber, text }) => (
-                    <div key={id} className="py-5 text-2xl text-center md:text-left">
-                        +
-                        <CountUp start={startNumber} end={endNumber} duration={1.5} enableScrollSpy />
-                        {" "}
-                        <span className="degradedBlue bg-blueLight">{text}</span>
-                    </div>
+                    <CounterItem key={id} startNumber={startNumber} endNumber={endNumber} text={text} />
                 ))}
             </div>
         </MotionTransition>
-    )
-}
+    );
+};
 
-export default CounterData
+export default CounterData;
+
+const CounterItem = ({ startNumber, endNumber, text }: { startNumber: number; endNumber: number; text: string }) => {
+    const { ref, inView } = useInView({ triggerOnce: true });
+
+    return (
+        <div ref={ref} className="py-5 text-2xl text-center md:text-left">
+            + {inView && <CountUp start={startNumber} end={endNumber} duration={1.5} />}{" "}
+            <span className="degradedBlue bg-blueLight">{text}</span>
+        </div>
+    );
+};
