9b313b19954531c0cd334654e072a082b87003e9
ivantm0 <iterolmartinez@gmail.com>
2025-04-03 11:40:15 +0200

    feat: add reveal animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import BestBusiness from "@/components/BestBusiness/BestBusiness";
+import Cards from "@/components/Cards/Card";
+import Billing from "@/components/Billing/Billing";
+import CounterData from "@/components/CounterData/CounterData";
+import FirstBlock from "@/components/FirstBlock/FirstBlock";
+import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import Payments from "@/components/Payments/Payments";
+import Testimonials from "@/components/Testimonials/Testimonials";
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
+      <FirstBlock />
+      <CounterData />
+      <BestBusiness />
+      <Cards />
+      <Billing />
+      <Testimonials />
+      <Payments />
+      <Footer />
     </>
-  );
+  )
 }
