f06f8e414aa0bfee973b49147a860af711e0fa8d
ivantm0 <iterolmartinez@gmail.com>
2025-04-03 11:26:21 +0200

    feat: add best business

---
+++ src/components/BestBusiness/BestBusiness.data.ts
@@ -0,0 +1,30 @@
+type BestBusinessData = {
+    id: number;
+    icon: string;
+    title: string;
+    description: string;
+  }[];
+
+export const dataFeaturesBusiness: BestBusinessData = [
+  {
+    id: 1,
+    icon: "rewards",
+    title: "Recompensas",
+    description:
+      "Las mejores tarjetas de crédito ofrecen algunas combinaciones tentadoras de promociones y premios.",
+  },
+  {
+    id: 2,
+    icon: "safe",
+    title: "100% Seguro",
+    description:
+      "Tomamos medidas proactivas para asegurarnos de que su información  y sus transacciones estén seguras.",
+  },
+  {
+    id: 3,
+    icon: "send",
+    title: "Envíos gratis",
+    description:
+      "Una tarjeta de crédito con transferencia de saldo puede ahorrarle mucho dinero en intereses.",
+  },
+];
